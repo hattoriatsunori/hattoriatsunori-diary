ad34bb9dc2adb9d0c396c4d3d3b32d0d4797f7ce
Atsunori Hattori <atsunori_hattori@imasen.co.jp>
2022-08-25 14:58:10 +0900

    firebaseアプリの初期設定を追加

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCCGdI0VmQL6qp2Y3vkBJsbdZeLTJ1aOWM",
+  authDomain: "hattoriatsunori-diary.firebaseapp.com",
+  projectId: "hattoriatsunori-diary",
+  storageBucket: "hattoriatsunori-diary.appspot.com",
+  messagingSenderId: "574178262692",
+  appId: "1:574178262692:web:17d0afd9e1613c81db3031",
+  measurementId: "G-059GKS53HP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if(typeof window !== 'undefined'){
+  const analytics = getAnalytics(app);
+}
